45a2cdc096d3d82d9b42ab3604babdea61c750f8
vy3191 <venkyafhs@gmail.com>
2019-09-21 23:54:42 -0400

    Modified Journal styles Modified Welcome styles

--- src/components/Journal/JournalEntries.js
+++ src/components/Journal/JournalEntries.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import {CardGroup, Card} from 'react-bootstrap';
 
 export default function JournalEntries() {
    return(
+     <div>
      <h2>Journal Entries </h2>
+     <CardGroup>
+        <Card>
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>      
+      </CardGroup>
+      </div>
    )
 }
--- src/components/Journal/Welcome.js
+++ src/components/Journal/Welcome.js
@@ -16,22 +16,24 @@ export default function Welcome() {
         <div className='journal-entries'>
             <h1>Journal Entries Journey</h1>
             <div className='journal-container'>           
-            <div className='journal-links'>            
-                <ul>
-                <li><NavLink to='/welcome/journal-entries'>View Your Recent Entries</NavLink></li>
-                <li><NavLink to='/welcome/ten-year-entries'>View Last Ten Years Entries</NavLink></li>
-                <li><NavLink to='/welcome/new-entries'>Add New Entries</NavLink></li> 
-                </ul>             
-                           
-            </div>
-            <div className='journals'>
-              <Switch>
-                <Route exact path='/welcome/journal-entries' render={(props) => <JournalEntries {...props}/>} />
-                <Route path='/welcome/ten-year-entries' render={(props) => <TenYearEntries {...props}/>} />
-                <Route path='/welcome/new-entries' render={(props) => <NewEntry /> } />
-                <Route render={(props) => <Image {...props} />} />
-              </Switch>
-            </div>   
+                <div className='journal-links'> 
+                     <h3>Your Actions</h3>  
+                     <hr style={{width:'100%'}}/> 
+                          
+                    <ul>
+                        <li><NavLink to='/welcome/journal-entries'>View Your Recent Entries</NavLink></li>
+                        <li><NavLink to='/welcome/ten-year-entries'>View Last Ten Years Entries</NavLink></li>
+                        <li><NavLink to='/welcome/new-entries'>Add New Entries</NavLink></li> 
+                    </ul>      
+                </div>
+                <div className='journals'>
+                    <Switch>
+                        <Route exact path='/welcome/journal-entries' render={(props) => <JournalEntries {...props}/>} />
+                        <Route path='/welcome/ten-year-entries' render={(props) => <TenYearEntries {...props}/>} />
+                        <Route path='/welcome/new-entries' render={(props) => <NewEntry /> } />
+                        <Route render={(props) => <Image {...props} />} />
+                    </Switch>
+                </div>   
             </div>         
         </div>
     )
